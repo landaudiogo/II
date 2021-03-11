277ad9d3b5d9385d16ae0db08664955a7b1114d9
landaudiogo <diogo.hewitt.landau@hotmail.com>
2021-03-11 13:53:59 +0000

    XML: modifying force_list logic

--- MES/mes_client/general/xml/HandleERPRequest.py
+++ MES/mes_client/general/xml/HandleERPRequest.py
@@ -2,31 +2,72 @@ import xmltodict
 from jsonpickle import decode
 import json
 
-def parseXML(xml_string, force_list=()) -> dict: 
+def parseXML(xml_string, force_list={}): 
+    """Função cujo objetivo é o de converter um objeto xml para um objeto
+    dictionary para ser posteriormente processado pela função de inicialização
+    das Classes relacionadas com a Base de Dados
+    
+    Argumentos
+    ----------
+
+    xml_string: a string xml que deve ser transformado num dicionário python
+    force_list: um dicionário que através das chaves indica que objetos, dentro
+        da string XML, devem ser transformados para listas. Útil para as relações
+        que temos definidas para um para muitos.
+        E.g. 
+            parseXML('''
+                <Order order_number='nnn'>
+                    <Transform .../>
+                    <Transform .../>
+                </Order>''',
+                force_list={'Transform': 'transformations'}
+            )
+
+    """
+
     xml_obj = decode(
         json.dumps(
-            xmltodict.parse(xml_string, force_list=force_list)
+            xmltodict.parse(xml_string, force_list=list(force_list.keys()))
         )
     )
-    return handle_object(xml_obj)
+    return handle_object(xml_obj, substitute_keys=force_list)
 
-def handle_object(obj):
+
+def handle_object(obj, substitute_keys={}):
+    """Formata `obj` por forma a que consiga ser processado pela
+    função de inicialização genérica.
+
+    Este processamento passa por remover characteres desconhecidos à função
+    ('@', '#'), e por alterar as chaves do dicionário para para os attributos
+    definidos em cada classe, relacionados com as colunas da base de dados.
+    """
+
     if isinstance(obj, dict):
         ret_dict = dict()
         for key, value in obj.items(): 
-            print(key, value)
             if key[0] == '#': 
                 continue
             elif key[0] == '@':
-                ret_dict[key[1:].lower()] = handle_object(value)
+                key = key[1:]
+                hash_key = (
+                    substitute_keys[key] 
+                    if substitute_keys.get(key) 
+                    else key.lower() 
+                )
+                ret_dict[hash_key] = handle_object(value, substitute_keys)
             else: 
-                ret_dict[key.lower()] = handle_object(value)
+                hash_key = (
+                    substitute_keys[key] 
+                    if substitute_keys.get(key) 
+                    else key.lower()
+                )
+                ret_dict[hash_key] = handle_object(value, substitute_keys)
         return ret_dict
 
     if isinstance(obj, list): 
         ret_list = []
         for item in obj: 
-            ret_list.append(handle_object(item))
+            ret_list.append(handle_object(item, substitute_keys))
         return ret_list
 
     else: 
--- README.md
+++ README.md
@@ -1,1 +1,42 @@
-# II
+### Preferências:
+
+Manu - PLC, MES
+Gabi - MES, PLC
+Ana - Indiferente
+Diogo - Indiferente
+
+
+### Diagramas (até 14.03)
+- Classes 
+	- Base de Dados
+	- Processo
+- Objetos
+- Sequência
+
+
+### PLC 
+- mapeamento de endereços para cada componente (Gabi)
+- desenho dos componentes simples (Diogo, Ana)
+- desenho complexos (Gabi, Ana, Manu)
+
+
+
+### MES 
+- Interações com base de dados (Diogo)
+- Comunicação com chão de fábrica (Manu)
+- Comunicação ERP (Diogo, Gabi)
+- Decisão de entrada de peça (Ana, Diogo)
+
+##### Interações com a base de dados: 
+Tarefas: 
+1. Desenho do diagrama de classes para a base de dados
+2. Mapeamento local Python da base de dados
+3. Funções genéricas simples: get, add, remove, update
+4. Funções genéricas complexas: 
+	- get_objects_using_attrs: where clause restricted to the attributes
+	- object_insert_or_nothing: insert an object in case it does not exist and in case it does exist don't do anything. All the nested objects are added in the same manner.
+5. Converter xml para json
+
+### Interface
+- Desenvolver API
+- Frontend
